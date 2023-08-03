96a0bdff300ba598d32e3490ae708aebaeb94851
Jose Guaro <gaurojose@gmail.com>
2023-08-03 00:00:06 -0700

    save point

--- app/src/main/java/jg/sh/SeaHorseInterpreter.java
+++ app/src/main/java/jg/sh/SeaHorseInterpreter.java
@@ -194,7 +194,7 @@ public class SeaHorseInterpreter {
     args = args == null ? new String[0] : args;
     
     LOG.info(new File("").getAbsolutePath());
-    String mainModule = "../sampleSrcs/fibb_sync.shr";
+    String mainModule = "../sampleSrcs/verysimple.shr";
     
     Map<IOption, Object> options = InterpreterOptions.getDefaultOptions();
     options.put(IOption.MEASURE, false);
